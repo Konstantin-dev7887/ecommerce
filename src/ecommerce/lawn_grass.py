c55d453fcf1e79265c15a8fc8b12318d81766826
konstantin <k35753367@gmail.com>
2026-02-24 21:59:03 +0300

    fix: fixed flake8

--- src/ecommerce/lawn_grass.py
+++ src/ecommerce/lawn_grass.py
@@ -5,16 +5,19 @@ from .product import Product
 
 class LawnGrass(Product):
     def __init__(
-        self,
-        name: str,
-        description: str,
-        price: float,
-        quantity: int,
-        country: str,
-        germination_period: int,
-        color: str,
+            self,
+            name: str,
+            description: str,
+            price: float,
+            quantity: int,
+            country: str,
+            germination_period: int,
+            color: str,
     ) -> None:
-        super().__init__(name=name, description=description, price=price, quantity=quantity)
+        super().__init__(name=name,
+                         description=description,
+                         price=price,
+                         quantity=quantity)
 
         self.country = country
         self.germination_period = germination_period
--- src/ecommerce/smartphone.py
+++ src/ecommerce/smartphone.py
@@ -5,17 +5,20 @@ from .product import Product
 
 class Smartphone(Product):
     def __init__(
-        self,
-        name: str,
-        description: str,
-        price: float,
-        quantity: int,
-        efficiency: float,
-        model: str,
-        memory: int,
-        color: str,
+            self,
+            name: str,
+            description: str,
+            price: float,
+            quantity: int,
+            efficiency: float,
+            model: str,
+            memory: int,
+            color: str,
     ) -> None:
-        super().__init__(name=name, description=description, price=price, quantity=quantity)
+        super().__init__(name=name,
+                         description=description,
+                         price=price,
+                         quantity=quantity)
 
         self.efficiency = efficiency
         self.model = model
